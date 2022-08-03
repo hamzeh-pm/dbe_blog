e3e5873fc35921841f2535b96b9359aa7a8d6227
hamzeh_pm <hpourmahdi@hotmail.com>
2022-08-03 12:17:28 +0430

    add admin table for blog post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "slug",
+        "author",
+        "published",
+        "status",
+    ]
