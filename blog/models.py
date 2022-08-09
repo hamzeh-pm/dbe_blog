e83aa480b3257222c92bee4f109863e217d721f3
hamzeh_pm <hpourmahdi@hotmail.com>
2022-08-09 20:30:21 +0430

    change - post model add - detail view add - list view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status="published")
 
 
 class Post(models.Model):
@@ -14,13 +20,19 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
-        ordering = ("-published",)
+        ordering = ("-publish",)
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
