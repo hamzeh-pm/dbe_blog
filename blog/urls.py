e3999fa6e2e74550c5a806c0a3ee68aa72901bf3
hamzeh_pm <hpourmahdi@hotmail.com>
2022-08-20 09:01:35 +0430

    add - postgresql search change - read me to more completed on

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("tags/<slug:tag_slug>/", views.PostList.as_view(), name="post_list_tag"),
     path("", views.PostList.as_view(), name="post_list"),
+    path("tags/<slug:tag_slug>/", views.PostList.as_view(), name="post_list_tag"),
+    path("search/", views.post_search, name="post_search"),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
     path("share/<int:post_id>/", views.post_share, name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from typing import Optional
 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+    TrigramSimilarity,
+)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -8,7 +13,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 
 
@@ -74,14 +79,6 @@ class PostList(ListView):
             tag = get_object_or_404(Tag, slug=tag_slug)
             return super().get_queryset().filter(tags__in=[tag])
 
-        if search_phrase:
-            return (
-                super()
-                .get_queryset()
-                .annotate(search_vector=SearchVector("title", "body"))
-                .filter(search_vector=search_phrase)
-            )
-
         return super().get_queryset()
 
     def get_context_data(self, **kwargs):
@@ -116,4 +113,38 @@ def post_share(request, post_id):
 
     return render(
         request, "blog/post/share.html", {"post": post, "form": form, "send": sent}
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            # search_vector = SearchVector("title", weight="A") + SearchVector(
+            #     "body", weight="B"
+            # )
+            # search_query = SearchQuery(query)
+
+            # results = (
+            #     Post.published.annotate(
+            #         search=search_vector, rank=SearchRank(search_vector, search_query)
+            #     )
+            #     .filter(rank__gte=0.3)
+            #     .order_by("-rank")
+            # )
+
+            results = (
+                Post.objects.annotate(similarity=TrigramSimilarity("title", query))
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
